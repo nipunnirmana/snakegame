4e79be27985c42d055e13adedbc2e4a7da62e39a
nipunnirmana <nipunfernando@hotmail.com>
2017-07-15 22:02:36 +0530

    var option added for readability

--- js/app.js
+++ js/app.js
@@ -1,45 +1,45 @@
-var gameCanvasID = 'game';
-var gH = 500;
-var gW = 500;
-var bW = 20;
-var bH = 20;
-var xPos = 0;
-var yPos = 0;
-var ctx , flowDirection;
-var speed = 100; // in ms
+var options = {
+  gameCanvasID : 'game', // Game canvas ID *
+  gH : 500, // Game Height
+  gW : 500, // Gamw Width
+  bW : 20, // Box Height
+  bH : 20, // Box Width
+  xPos : 0, // Starting x Position
+  yPos : 0, // Starting y Position
+  speed : 100 , // Speed in ms
+  ctx : "", // Context of canavas leave blank
+  flowDirection : "" // Flow interval leave blank
+};
 
 function initGame() {
-  var game = document.getElementById(gameCanvasID);
-  ctx = game.getContext("2d");
-  game.height = gH;
-  game.width = gW;
+  var game = document.getElementById(options.gameCanvasID);
+  options.ctx = game.getContext("2d");
+  game.height = options.gH;
+  game.width = options.gW;
   moveBox();
   flow(39);
 }
 
-function moveBox(direction) {
+function moveBox(direction) { // Move box in given direction
 
-  ctx.clearRect(0, 0, gW, gH);
-  if (direction === 37) (xPos-bH < 0) ? xPos = gW-bW : xPos = xPos-bW ; // LEFT
-  if (direction === 38) (yPos-bW < 0) ? yPos = gH-bH : yPos = yPos-bH ; // UP
-  if (direction === 39) (xPos+bH > gW-bW) ? xPos = 0 : xPos = xPos+bW; // RIGHT
-  if (direction === 40) (yPos+bH > gH-bH) ? yPos = 0 : yPos = yPos+bH ; // DOWN
+  options.ctx.clearRect(0, 0, options.gW, options.gH);
+  if (direction === 37) (options.xPos-options.bH < 0) ? options.xPos = options.gW-options.bW : options.xPos = options.xPos-options.bW ; // LEFT
+  if (direction === 38) (options.yPos-options.bW < 0) ? options.yPos = options.gH-options.bH : options.yPos = options.yPos-options.bH ; // UP
+  if (direction === 39) (options.xPos+options.bH > options.gW-options.bW) ? options.xPos = 0 : options.xPos = options.xPos+options.bW; // RIGHT
+  if (direction === 40) (options.yPos+options.bH > options.gH-options.bH) ? options.yPos = 0 : options.yPos = options.yPos+options.bH ; // DOWN
 
-  ctx.clearRect(0, 0, gW, gH);
-  ctx.fillRect(xPos,yPos,bW,bH);
+  options.ctx.clearRect(0, 0, options.gW, options.gH);
+  options.ctx.fillRect(options.xPos,options.yPos,options.bW,options.bH);
 
 }
 
-function flow(code) {
-  clearInterval(flowDirection);
-
-  flowDirection = setInterval(function functionName() {
-    moveBox(code);
-  },speed);
+function flow(code) { // Move box until user changes box direction
+  clearInterval(options.flowDirection);
+  options.flowDirection = setInterval(function() {moveBox(code);},options.speed);
 }
 
 
-document.addEventListener('keydown',function (e) {
+document.addEventListener('keydown',function (e) { // Bind keys
   moveBox(e.keyCode);
   flow(e.keyCode);
 });
